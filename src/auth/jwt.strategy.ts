906273a438a551073a408b180fe3c83bef388c38
zim89 <121241841+zim89@users.noreply.github.com>
2026-08-20 12:01:23 +0300

    feat/configuration
    Integrate @nestjs/config for environment variable management and enhance JWT configuration
    
    - Added @nestjs/config and updated app.module.ts to load environment variables using ConfigModule.
    - Refactored TypeOrmModule and JwtModule to use asynchronous configuration with ConfigService.
    - Updated package.json and bun.lock to include new dependencies: @nestjs/config, joi, and dotenv.
    - Improved application startup scripts to set the environment stage for development and production.

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,14 +6,16 @@ import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { JwtPayload } from './jwt-payload.interface';
 import { JwtUser } from './jwt-user.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret512',
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
